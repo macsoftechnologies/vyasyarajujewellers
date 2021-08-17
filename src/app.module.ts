aba3be6db457a3acdeddd730e0a6076a7c3cc090
Macsoftechnologies <macsoftechnologies@gmail.com>
2021-08-17 10:49:21 +0530

    complete code

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,16 +16,17 @@ import { imagesModule } from './images/images.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({type: 'mysql',
-       host: 'sql6.freesqldatabase.com',
+       host: 'ls-9b6b93368d6c8ba4138f6ccb36923e6c27c074a4.c7vrc7k2qb9e.ap-south-1.rds.amazonaws.com',
        port: 3306,
-       username: 'sql6429236',
-       password: '76gj3NCiJS',
-       database: 'sql6429236',
+       username: 'dbmasteruser',
+       password: '5016:loqLAL,(]FfeVO9qMRt.8kYD<M7',
+       database: 'dbmaster',
        logging:true,
-       synchronize: true,
+      synchronize: true,
        autoLoadEntities: true,
-     // keepConnectionAlive: true,
-      //insecureAuth : true
+      keepConnectionAlive: true,
+      retryDelay: 3000,
+      insecureAuth : true
    }),
       userRegisterModule,
       queriesModule,
